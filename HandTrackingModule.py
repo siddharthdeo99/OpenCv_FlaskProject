8b5ab8ef45a4273ecd658b19caf49d1f62fe437d
Siddharth <siddharthlaldeo@gmail.com>
2021-11-03 17:04:28 +0530

    v1.1

---
+++ HandTrackingModule.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+
+class handDetector():
+    def __init__(self,mode=False, maxHands=2, detectionCon=0.5,trackCon=0.5):
+        self.mode=mode
+        self.maxHands= maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands= mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+    
+    def findHands(self, img,draw=True):
+
+
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        
+        return img   
+
+    def findPosition(self,img,handNo=0 , draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myhand=self.results.multi_hand_landmarks[handNo]
+
+            for id,lm in enumerate(myhand.landmark):
+                h,w,c =img.shape
+                cx,cy=int(lm.x * w),int(lm.y *h)
+                # print(id,cx,cy)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+        return lmList
+
+
+def main():
+    cap= cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success , img = cap.read()
+        img=detector.findHands(img)
+        lmList= detector.findPosition(img)
+        if len(lmList)!=0:
+            print(lmList[4])
+        cv2.imshow("Image",img)
+        cv2.waitKey(1)
+
+
+
+if __name__== "__main__":
+    main()
